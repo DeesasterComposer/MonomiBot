97e41254d36cf1227e62e88f3b2d6e28ae64a160
DeesasterComposer <boondinisdipperis@gmail.com>
2020-05-06 03:45:07 -0500

    Update monomi.js

--- monomi.js
+++ monomi.js
@@ -194,7 +194,7 @@ function recallStatusShuffle() {
 }
 
 /*Important Info*/
-var version = "3.6.1.2-8";
+var version = "3.6.1.2-9";
 var shuffleState = 0;
 var mouseTriggers = 0;
 var ottoTriggers = 0;
@@ -2818,11 +2818,11 @@ client.on("message", (message) => { //When a message is sent.
 		//The MATH
 		let loveVariableA = client.users.find('id', message.mentions.members.first().id).discriminator;
 		let loveVariableB = client.users.find('id', message.mentions.members.last().id).discriminator;
-		let loveFactor = (int(loveVariableA)+int(loveVariableB))/2;
+		let loveFactor = (Integer.parseInt(loveVariableA)+Integer.parseInt(loveVariableB))/2;
 		message.channel.send(`Love Factor: ${loveFactor}`);
-		let loveRating = int(str(loveFactor).slice(0, 1)) + int(str(loveFactor).slice(2, 3));
-		message.channel.send(`Love Partial A: ${int(str(loveFactor).slice(0, 1))}`);
-		message.channel.send(`Love Partial B: ${int(str(loveFactor).slice(2, 3))}`);
+		let loveRating = Integer.parseInt(str(loveFactor).slice(0, 1)) + Integer.parseInt(str(loveFactor).slice(2, 3));
+		message.channel.send(`Love Partial A: ${Integer.parseInt(str(loveFactor).slice(0, 1))}`);
+		message.channel.send(`Love Partial B: ${Integer.parseInt(str(loveFactor).slice(2, 3))}`);
 		message.channel.send(`Love Rating: ${loveRating}`);
 
 		let loveMeter = meters[loveRating - 1];
